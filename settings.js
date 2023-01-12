4f719a2e8054c2febb8584ee7eb8b8ea10914c68
CREATOR BOT WA <98082226+LynnXzy@users.noreply.github.com>
2023-01-13 00:48:04 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,7 +17,7 @@ global.APIKeys = {
 }
 
 // setting 
-global.autoread = false // auto read message
+global.autoread = true // auto read message
 global.autobio = false //auto ganti bio
 global.anticall = true //anti call 
 global.userRegister = false
@@ -31,24 +31,24 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/@ChZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
+global.botname = "Lynn-Mdོ" //namabot kalian
+global.ownername = "OwnerBotzLyn" //nama kalian
+global.myweb = "https://api-mirip-zeks.lynnxzy.repl.co/" //bebas asal jan hapus
+global.youtube = "https://youtube.com/channel/UC3Y7rHuI_OmbvFdL88JF7AQ" //bebas asal jan hapus
+global.myweb2 = "https://api-mirip-zeks.lynnxzy.repl.co/" //bebas
+global.email = "botzlynx@gmail.com" //bebas
+global.region = "Malaysia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.premium = ['60189830350'] //premium user
+global.owner = ["60189830350", "60149431385"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "60189830350" // nomor wa kalian
+global.ownernomerr = "+60189830350" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© Botz Wa' //sticker wm ubah
+global.author = 'Lynn-Md🐸🍭' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
